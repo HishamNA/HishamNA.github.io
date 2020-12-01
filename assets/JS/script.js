e8428a9314a997674581f48c0392c09509bb1537
Hisham Alsohybe <63221432+HishamNA@users.noreply.github.com>
2020-11-30 21:59:47 -0800

    hi

---
+++ assets/JS/script.js
@@ -0,0 +1,35 @@
+
+document.querySelector(".SVG-plus-1").onclick = function() {
+  document.querySelector("SVG").classList.toggle("SVG-rotate");
+};
+document.querySelector(".SVG-plus-2").onclick = function() {
+  document.querySelector(".SVG-plus-2").classList.toggle("SVG-rotate");
+};
+
+document.querySelector(".SVG-plus-3").onclick = function() {
+  document.querySelector(".SVG-plus-3").classList.toggle("SVG-rotate");
+};
+
+document.querySelector(".check").onclick = function(){
+  document.querySelector(".li1").classList.toggle("fade");
+};
+
+document.querySelector(".check-2").onclick = function(){
+  document.querySelector(".li2").classList.toggle("fade");
+};
+
+document.querySelector(".check-3").onclick = function(){
+  document.querySelector(".li3").classList.toggle("fade");
+};
+
+document.querySelector(".check-4").onclick = function(){
+  document.querySelector(".li4").classList.toggle("fade");
+};
+
+document.querySelector(".check-5").onclick = function(){
+  document.querySelector(".li5").classList.toggle("fade");
+};
+
+document.querySelector(".check-6").onclick = function(){
+  document.querySelector(".li6").classList.toggle("fade");
+};
